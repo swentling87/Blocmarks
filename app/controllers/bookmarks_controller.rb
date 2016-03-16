e7432ed7a3ff7f6278e765acfa6f32ae5350d912
Stephen Wentling <swentling87@gmail.com>
2016-03-15 23:08:34 -0700

    embedly added

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -68,4 +68,5 @@ class BookmarksController < ApplicationController
     flash[:alert] = "You are not authorized to perform this action."
     redirect_to(request.referrer || root_path)
   end
+
 end
